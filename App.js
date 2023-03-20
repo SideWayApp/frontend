6175187ce95ac478f4ccb956f5a6d76f255c7f7f
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-20 16:18:55 +0200

    added sign in screen

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import HomeScreen from "./pages/HomeScreen";
 import SignInScreen from "./pages/SignInScreen";
 import AppBarComponent from "./components/AppBarComponent";
--- pages/SignInScreen.js
+++ pages/SignInScreen.js
@@ -1,12 +1,109 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 function SignInScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    // Implement your sign-in logic here
+    console.log("Signing in with email:", email, "and password:", password);
+  };
+
+  const handleForgotPassword = () => {
+    // handle forgot password logic here
+  };
+
+  const handleSignUp = () => {
+    // handle sign-up logic here
+  };
+
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign In</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.forgotPasswordButton}
+        onPress={handleForgotPassword}
+      >
+        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
+        <Text style={styles.signUpText}>Don't have an account? Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    // justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "blue",
+    width: "100%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+  forgotPasswordButton: {
+    marginTop: 10,
+  },
+  forgotPasswordText: {
+    color: "blue",
+    fontSize: 14,
+  },
+  signUpButton: {
+    marginTop: 20,
+  },
+  signUpText: {
+    color: "black",
+    fontSize: 16,
+    textDecorationLine: "underline",
+  },
+});
 
 export default SignInScreen;
