fd4c6104a7d40cc4f93358bf7b7869a8d7d5f2ab
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-04-07 15:03:17 +0300

    changed viewed delta and add callout to marker

--- components/MapItemsComponent.js
+++ components/MapItemsComponent.js
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Marker } from "react-native-maps";
+import { Marker, Callout } from "react-native-maps";
 import { fetchObjectsInRegion } from "../axios";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 // "alert-octagon", "camera"
+
+const minDisplayDelta = {
+  latitudeDelta: 0.005,
+  longitudeDelta: 0.005,
+};
+
 export default function MapItemsComponent({ region }) {
   const [mapItems, setMapItem] = useState([]);
 
@@ -13,24 +19,64 @@ export default function MapItemsComponent({ region }) {
       const objects = await fetchObjectsInRegion(region);
       setMapItem(objects);
     };
-
-    fetchObjects();
+    console.log(region);
+    if (
+      region.latitudeDelta <= minDisplayDelta.latitudeDelta ||
+      region.longitudeDelta <= minDisplayDelta.longitudeDelta
+    ) {
+      fetchObjects();
+    } else {
+      setMapItem([]);
+    }
   }, [region]);
   return (
     <>
       {mapItems.map((mapItem, index) => {
         return (
           <Marker
+            title={mapItem.type}
+            description={mapItem.streetName}
             key={mapItem.y + mapItem.x}
             coordinate={{ latitude: mapItem.y, longitude: mapItem.x }}
           >
-            <View style={{ width: 50, height: 50 }}>
+            <View style={styles.markerContainer}>
               {mapItem.type === "camera" && <Icon name="camera" />}
               {mapItem.type !== "camera" && <Icon name="alert-octagon" />}
             </View>
+            <Callout tooltip>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutTitle}>{mapItem.type}</Text>
+                <Text style={styles.calloutDescription}>
+                  {mapItem.streetName}
+                </Text>
+              </View>
+            </Callout>
           </Marker>
         );
       })}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  markerContainer: {
+    backgroundColor: "#fff",
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 5,
+  },
+  calloutContainer: {
+    backgroundColor: "gray",
+    borderRadius: 5,
+    width: 150,
+  },
+  calloutTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  calloutDescription: {
+    fontSize: 14,
+  },
+});
