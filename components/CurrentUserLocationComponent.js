999820a3f077d770021ab6adf0ceb86a4b6cdcdc
Roy Ambar <93988278+royambar1@users.noreply.github.com>
2023-05-10 16:53:35 +0300

    live location person

--- components/CurrentUserLocationComponent.js
+++ components/CurrentUserLocationComponent.js
@@ -1,34 +1,33 @@
-import { View, Text ,StyleSheet} from 'react-native'
-import React from 'react'
-import {Marker} from 'react-native-maps';
-import Icon from "@expo/vector-icons/MaterialCommunityIcons"
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Marker } from "react-native-maps";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function CurrentUserLocationComponent({location}){
+export default function CurrentUserLocationComponent({ location }) {
   return (
     <Marker
-        coordinate={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-        }}
-        title="My Phone">
-        <View style={styles.markerContainer}>
-            <Icon name="walk" size={40} color="black"/>
-        </View>
+      coordinate={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+      }}
+    >
+      <FontAwesome name="male" size={24} color="black" />
     </Marker>
-  )
+  );
 }
 const styles = StyleSheet.create({
   markerContainer: {
     borderColor: "#000",
     padding: 0,
-    borderRadius: 100
+    borderRadius: 100,
   },
   iconContainer: {
-    justifyContent: 'center',
+    justifyContent: "center",
     width: 30,
     height: 30,
-    bottom:3,
-    alignSelf:'center',
-    alignItems: 'center',
+    bottom: 3,
+    alignSelf: "center",
+    alignItems: "center",
   },
 });
