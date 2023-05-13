7f823d3b443930b16e48e97b2f8031071aeb0847
Yuvalkop22 <Kopelman050@gmail.com>
2023-05-13 12:10:05 +0300

    line's color per preference

--- components/OnMapDirections.js
+++ components/OnMapDirections.js
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 import { Polyline } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import polyline from "@mapbox/polyline";
+import { useSelector } from "react-redux";
 
 export default function OnMapDirections({ wayPoints, polylinePoints }) {
   const [routeCoordinates, setRouteCoordinates] = useState([]);
+  const user = useSelector((state) => state.auth.user);
+  const [LineColor, setLineColor] = useState("gray");
+
   useEffect(() => {
     if (polylinePoints) {
       const decodedPoints = polyline.decode(polylinePoints, {
@@ -15,17 +19,35 @@ export default function OnMapDirections({ wayPoints, polylinePoints }) {
         latitude: point[0],
         longitude: point[1],
       }));
-      console.log(coordinates.length);
       setRouteCoordinates(coordinates);
     }
   }, [polylinePoints]);
+
+  useEffect(() => {
+    if (user && user.preferences) {
+      const { accessibility, clean, scenery, security, speed } = user.preferences;
+      const colorMapping = {
+        accessibility: "green",
+        clean: "blue",
+        scenery: "yellow",
+        security: "orange",
+        speed: "red",
+      };
+
+      Object.keys(user.preferences).forEach((option) => {
+        if (user.preferences[option]) {
+          setLineColor(colorMapping[option]);
+        }
+      });
+    }
+  }, [user]);
+
   return (
     <Polyline
       coordinates={routeCoordinates}
-      // apikey={GOOGLE_API_KEY}
       strokeWidth={3}
       mode="WALKING"
-      strokeColor="red"
+      strokeColor={LineColor}
     />
   );
 }
