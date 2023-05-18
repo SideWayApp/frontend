4c7e23cf3e46f3a1ddb9ad8b94d51021a83c08c7
gal levy <gallevy34@gmail.com>
2023-05-18 20:01:43 +0300

    update callouts for mapItems

--- components/MapItemsComponent.js
+++ components/MapItemsComponent.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Alert, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Marker, Callout } from "react-native-maps";
 import { fetchObjectsInRegion } from "../axios";
@@ -14,6 +14,10 @@ const minDisplayDelta = {
 export default function MapItemsComponent({ region }) {
   const [mapItems, setMapItem] = useState([]);
   const user = useSelector((state) => state.auth.user);
+
+  const updateReport = (type) =>{
+    console.log("yeaaaaaaaaaaaaaaaaaaaa")
+  }
   useEffect(() => {
     // Fetch objects based on the current region
     const fetchObjects = async (preferences) => {
@@ -47,9 +51,25 @@ export default function MapItemsComponent({ region }) {
             <Callout tooltip>
               <View style={styles.calloutContainer}>
                 <Text style={styles.calloutTitle}>{mapItem.type}</Text>
-                <Text style={styles.calloutDescription}>
-                  {mapItem.streetName}
-                </Text>
+                <Text style={styles.calloutQuestion}>is it still there?</Text>
+                <View  style={styles.container}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                      updateReport(mapItem.type)
+                      }
+                    }>
+                      <Text style={styles.textStyle}>Yes</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                      
+                      }
+                    }>
+                      <Text style={styles.textStyle}>No</Text>
+                  </TouchableOpacity>
+                </View>           
               </View>
             </Callout>
           </Marker>
@@ -75,9 +95,36 @@ const styles = StyleSheet.create({
   calloutTitle: {
     fontWeight: "bold",
     fontSize: 16,
+    marginBottom: 2.5,
+    textAlign:"center"
+  },
+  calloutQuestion: {
+    fontSize: 13,
     marginBottom: 5,
+    textAlign:"center"
   },
   calloutDescription: {
     fontSize: 14,
   },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    },
+    button: {
+      backgroundColor: '#2196F3',
+      width: '40%',
+      height: 35,
+      borderRadius: 20,
+      padding: 6,
+      elevation: 2,
+      marginBottom: 5,
+      marginLeft:8,
+      marginRight:8,
+    },
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
 });
